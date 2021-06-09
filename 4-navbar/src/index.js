690665eb15a6677e986e9ffed3ae7826b877db89
Jon Rutter <69657008+jonrutter@users.noreply.github.com>
2021-06-09 15:45:44 -0400

    Styling fixes
    Repaired some styling issues

--- 4-navbar/src/index.js
+++ 4-navbar/src/index.js
@@ -2,6 +2,7 @@ const navTrigger = document.querySelector('#nav-trigger');
 const navIcon = document.querySelector('.nav__button-icon');
 const navList = document.querySelector('#nav-dropdown');
 
+// Need to add aria and tabindex changes for accessibility
 function navOpen() {
   navList.classList.add('nav__list--active');
   navIcon.textContent = 'close';
