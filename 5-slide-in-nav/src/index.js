44ca28f492ced0456ded09b671e0416d42ed9d2f
Jon Rutter <69657008+jonrutter@users.noreply.github.com>
2021-06-09 18:19:38 -0400

    Accessibility fixes

--- 5-slide-in-nav/src/index.js
+++ 5-slide-in-nav/src/index.js
@@ -4,8 +4,7 @@ const body = document.querySelector('body');
 const navOpen = document.querySelector('.nav__open');
 const navClose = document.querySelector('.nav__close');
 
-const mainTabs = document.querySelector('[tabindex="-1"]');
-const navTabs = document.querySelector('[tabindex="0"]');
+const tabs = document.querySelectorAll('[tabindex]');
 
 // Nav Functions
 
@@ -14,14 +13,48 @@ function isNavOpen() {
   return nav.classList.contains('nav--open');
 }
 
+// toggles all elements with an attribute of tabindex
+// if tabindex="0", sets to tabindex="-1", and vice-versa
+function toggleTabs() {
+  tabs.forEach((tab) => {
+    if (tab.getAttribute('tabindex') === '0') {
+      tab.setAttribute('tabindex', '-1');
+    } else {
+      tab.setAttribute('tabindex', 0);
+    }
+  });
+}
+
 function openNav() {
   nav.classList.add('nav--open');
   body.classList.add('no-scroll');
+
+  // aria attributes
+  nav.removeAttribute('aria-hidden');
+  navOpen.setAttribute('aria-expanded', 'true');
+
+  // tabindex
+  // when the nav menu is opened, users should be able to tab to the links inside
+  toggleTabs();
+
+  // focusing nav close button, to take focus off the navOpen if user tabbed to it
+  navClose.focus();
 }
 
 function closeNav() {
   nav.classList.remove('nav--open');
   body.classList.remove('no-scroll');
+
+  // aria attributes
+  nav.setAttribute('aria-hidden', 'true');
+  navOpen.removeAttribute('aria-expanded');
+
+  // tabindex
+  // when the nav menu is closed, users should not be able to tab to the links inside
+  toggleTabs();
+
+  // focusing nav open button, to take focus off the navClose if user tabbed to it
+  navOpen.focus();
 }
 
 // Event Listeners
